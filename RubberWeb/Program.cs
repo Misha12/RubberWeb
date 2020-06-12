2c0928247ab7f565f18afaeb333689aa4af8baac
MichalHalabica <m.halabica@gmail.com>
2020-06-12 23:11:00 +0200

    Allow define port.

--- RubberWeb/Program.cs
+++ RubberWeb/Program.cs
@@ -8,7 +8,15 @@ namespace RubberWeb
         public static void Main(string[] args)
         {
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(builder => builder.UseStartup<Startup>())
+                .ConfigureWebHostDefaults(builder =>
+                {
+                    builder.UseStartup<Startup>();
+
+                    if(args.Length > 0 && int.TryParse(args[0], out int port))
+                    {
+                        builder.UseUrls($"http://127.0.0.1:{port}");
+                    }
+                })
                 .Build()
                 .Run();
         }
