c33d3e054d024a48832ee43725b09835b0b2cd8d
MichalHalabica <m.halabica@gmail.com>
2020-06-12 22:57:59 +0200

    Add stackoverflow link

--- RubberWeb/ClientApp/src/services/number-format.pipe.ts
+++ RubberWeb/ClientApp/src/services/number-format.pipe.ts
@@ -5,6 +5,7 @@ import { PipeTransform, Pipe } from '@angular/core';
 })
 export class NumberFormatPipe implements PipeTransform {
     transform(value: number, ...args: any[]) {
+        // see: https://stackoverflow.com/a/54036800
         return String(value).replace(/(?!^)(?=(?:\d{3})+$)/g, ' ');
     }
 }
