be20b07be833d94b66b7ad2cd22a85c5c0fc12ec
MichalHalabica <m.halabica@gmail.com>
2020-06-12 17:49:48 +0200

    Styles and text to loader

--- RubberWeb/ClientApp/src/app/loader.component.ts
+++ RubberWeb/ClientApp/src/app/loader.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-loader',
-    template: '<img src="/assets/duck.gif" width="256" height="256" />'
+    template: '<img src="/assets/duck.gif" width="256" height="256" /> <h1>Loading...</h1>',
+    styles: [
+        'img { display: block; }',
+        'h1 { text-align: center; font-weight: 100 }'
+    ]
 })
 export class LoaderComponent { }
