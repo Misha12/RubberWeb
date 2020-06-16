de72ee0900c4de37146d98478f9895e3c6910289
MichalHalabica <m.halabica@gmail.com>
2020-06-16 18:48:39 +0200

    Responsive web

--- RubberWeb/ClientApp/src/app/loader.component.ts
+++ RubberWeb/ClientApp/src/app/loader.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-loader',
-    template: '<img src="/assets/duck.gif" width="256" height="256" /> <h1>Načítání...</h1>',
+    template: '<div class="loader-img"><img src="/assets/duck.gif" width="256" height="256" /></div><h1>Načítání...</h1>',
     styles: [
+        '.loader-img { display: flex; justify-content: center; }',
         'img { display: block; }',
-        'h1 { text-align: center; font-weight: 100; color: #99AAB5 }'
+        'h1 { font-weight: 100; color: #99AAB5; text-align: center }'
     ]
 })
 export class LoaderComponent { }
--- RubberWeb/ClientApp/src/app/main/main.component.ts
+++ RubberWeb/ClientApp/src/app/main/main.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit, HostListener } from '@angular/core';
 import { PaginatedData } from 'src/models/pagination';
 import { KarmaItem } from 'src/models/karma';
 
@@ -7,16 +7,40 @@ import { KarmaItem } from 'src/models/karma';
     templateUrl: './main.component.html',
     styleUrls: ['./main.component.scss']
 })
-export class MainComponent {
+export class MainComponent implements OnInit {
     @Input() data: PaginatedData<KarmaItem>;
     @Input() currentPage = 1;
 
     @Output() pageChanged = new EventEmitter<number>();
 
     loading = false;
+    maxSize = 0;
 
     onPageChange(event: any) {
         this.pageChanged.emit(event);
         this.loading = true;
+    }
+
+    ngOnInit() {
+        this.maxSize = this.computeMaxSize();
+    }
+
+    @HostListener('window:resize', ['$event'])
+    onResize(_: any) {
+        this.maxSize = this.computeMaxSize();
+    }
+
+    computeMaxSize() {
+        const width = document.body.clientWidth;
+
+        if (width < 700) {
+            return 2;
+        } else if (width < 1000) {
+            return 10;
+        } else if (width < 1500) {
+            return 20;
+        } else {
+            return 50;
+        }
     }
 }
